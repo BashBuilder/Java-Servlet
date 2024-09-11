6aa091b1f19832d7988a62b1a86356306c5761db
unknown <iamadelowotan@gmail.com>
2024-09-11 19:22:14 +0100

    annotations used to replace the xml configurations

--- src/main/java/com/servlet/AddServlet.java
+++ src/main/java/com/servlet/AddServlet.java
@@ -2,14 +2,15 @@ package com.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
-//	public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
 	public void doGet( HttpServletRequest req, HttpServletResponse res ) throws IOException, ServletException {
 		int num1 = Integer.parseInt(req.getParameter("num1"));
 		int num2 = Integer.parseInt(req.getParameter("num2"));
@@ -20,8 +21,11 @@ public class AddServlet extends HttpServlet {
 		
 		PrintWriter outPrintWriter = res.getWriter();
 		
-		RequestDispatcher rd = req.getRequestDispatcher("square");
-		rd.forward(req, res);
+		HttpSession session = req.getSession();
+		session.setAttribute("num", result);
+		
+		
+		res.sendRedirect("square?num="+result);
 		
 		outPrintWriter.println(result);		
 	}
--- src/main/java/com/servlet/SquareServlet.java
+++ src/main/java/com/servlet/SquareServlet.java
@@ -3,16 +3,26 @@ package com.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
-
+@WebServlet("/square")
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+//		int num = Integer.parseInt(req.getParameter("num"));
+
+		HttpSession session = req.getSession();
+		int num = (int) session.getAttribute("num");
+		
+		int result = num * num;
+		
 		PrintWriter outPrintWriter = res.getWriter();
-		outPrintWriter.println("hello world");
+		outPrintWriter.println("The result is " + result);
 		
 	}
 }
